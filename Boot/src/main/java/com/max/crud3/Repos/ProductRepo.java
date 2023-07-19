1b59cf586a794e4bceab9ddd7c35c4560721e4a9
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2023-07-19 12:37:38 +0530

    Delete Boot directory

--- Boot/src/main/java/com/max/crud3/Repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.max.crud3.Repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.max.crud3.Models.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}

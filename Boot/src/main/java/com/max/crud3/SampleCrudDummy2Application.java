022505df57af70fe248e11ed86ec7dfeaed2a2b5
Mohanraj M <126415700+MaX-NeO@users.noreply.github.com>
2023-07-19 12:36:01 +0530

    init

---
+++ Boot/src/main/java/com/max/crud3/SampleCrudDummy2Application.java
@@ -0,0 +1,13 @@
+package com.max.crud3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCrudDummy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleCrudDummy2Application.class, args);
+	}
+
+}
